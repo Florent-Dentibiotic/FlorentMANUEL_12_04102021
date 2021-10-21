1fe1dba9e7bb6491ed8de4837680033936c0d486
MacbookAir <fmdentiste@pm.me>
2021-10-21 15:21:58 +0200

    keyData JS

--- src/mapper/UserKeyDataMapper.js
+++ src/mapper/UserKeyDataMapper.js
@@ -1,6 +1,12 @@
 import UserKeyData from '../core/UserKeyData'
 
 export default class UserKeyDataMapper {
+    /**
+     * Convert received Json to new UserKeyData type
+     *
+     * @param { object } json
+     * @returns { new UserKeyData }
+     */
     static convertToKeyData(json) {
         return new UserKeyData(
             json.id,
